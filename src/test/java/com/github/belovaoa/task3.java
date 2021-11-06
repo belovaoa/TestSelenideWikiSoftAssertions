270f54675fffdf420d062c4202434ad3a4e98542
belova_oa <belovva.o@gmail.com>
2021-11-06 21:11:26 +0300

    add task3.java

---
+++ src/test/java/com/github/belovaoa/task3.java
@@ -0,0 +1,22 @@
+package com.github.belovaoa;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class task3 {
+
+    @Test
+    void workDragAndDrop() {
+
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a header").shouldHave(text("A"));
+        $("#column-b header").shouldHave(text("B"));
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a header").shouldHave(text("B"));
+        $("#column-b header").shouldHave(text("A"));
+
+    }
+}
