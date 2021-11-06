7acc1955fd7637ea560ea4e96c663249fc5580df
belova_oa <belovva.o@gmail.com>
2021-11-06 20:46:53 +0300

    add findExampleForJUnit.java

---
+++ src/test/java/com/github/belovaoa/findExampleForJUnit.java
@@ -0,0 +1,34 @@
+package com.github.belovaoa;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class findExampleForJUnit {
+
+
+    @Test
+    void findExample() {
+
+        // Open page selenide in github
+        open("https://github.com/selenide/selenide");
+
+        // go on page wiki of this project
+        $("#wiki-tab").click();
+
+        // on this page should be SoftAssertions
+        $(".markdown-body").shouldHave(text("Soft assertions"));
+
+        // open page SoftAssertions
+        $(".markdown-body").$(byText("Soft assertions")).click();
+
+        // this is page SoftAssertions
+        $("#wiki-wrapper").shouldHave(text("SoftAssertions"));
+
+        // here should be visible example for JUnit5
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
+
+    }
+}
