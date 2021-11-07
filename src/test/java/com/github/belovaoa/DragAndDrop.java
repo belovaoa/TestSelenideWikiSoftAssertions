3d07a280dda10dcf64076922d05d733cadbb1d65
belova_oa <belovva.o@gmail.com>
2021-11-07 08:06:21 +0300

    add DragAndDrop.java

--- src/test/java/com/github/belovaoa/DragAndDrop.java
+++ src/test/java/com/github/belovaoa/DragAndDrop.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class Task3 {
+public class DragAndDrop {
 
     @Test
     void workDragAndDrop() {
